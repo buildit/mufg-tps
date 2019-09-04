330275b154cf460de490c3675e2e20957522cd7a
Joe Stevens <joemac79@gmail.com>
2019-09-04 15:50:03 -0400

    Static Pages converted from markdown (#4)

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default () => <div>
+<h3 id="toc_0">To contact an MUFG Supply Chain Finance Professional</h3>
+
+<p>To learn more about how your company can benefit from MUFG’s Supply Chain Finance program, please complete the form below, and be certain to include a work email, rather than a personal one. </p>
+
+<p>One of our Supply Chain Finance Professionals will contact you shortly to discuss your interest/eligibility to participate in our program. </p>
+
+<ul>
+<li>Name:</li>
+<li>Job Function:</li>
+<li>Company:</li>
+<li>Work Email:</li>
+<li>Location (State, Territory, Country)</li>
+<li>CAPTCHA text: </li>
+</ul>
+
+<p>If you are an existing supplier and require support, please 
+<a href="mailto:SCFsupport@us.mufg.jp">SCFsupport@us.mufg.jp</a></p>
+
+<p>or call Customer Support Toll Free: +1.800.782.4082, Ext. 4</p>
+</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,27 @@
 import React from "react"
-import {Header} from '../components'
 
-export default () => (
-  <Header />
+export default () => <div>
+<h1 id="toc_10">Sidebar1 - Login</h1>
 
-)
+<ul>
+<li>Honeywell Suppliers, click here</li>
+<li>European Replenishment Suppliers, click here</li>
+<li>Suppliers using MUFG Platform, click here</li>
+<li>Activate (First Time Use Only)</li>
+<li>Forgot Password</li>
+</ul>
+
+<h1 id="toc_11">Sidebar2 - Supplier EducationCenter</h1>
+
+<p>See how Supply Chain Finance works and calculate the benefits of managing your working capital with SCF [Learn More] [link to What We Do page]</p>
+
+<h1 id="toc_12">Sidebar3 - What You Need to Know</h1>
+
+<p>Earlier this year, MUFG purchased the Trade Payables Services (TPS) platform from GE Capital. GE Capital’s early-payment plan to suppliers will end on [INSERT DATE]. Suppliers to GE businesses are invited to join the MUFG Supply Chain Finance Program. [Learn More] [link to What We Do page]</p>
+
+<h2 id="toc_13">Home</h2>
+
+<p>Looking to extract value from your supply chain and improve your working capital? </p>
+
+<p>Find the solutions you need to contain costs in your supply chain, improve working capital and free up liquidity in your cash conversion cycle. Allow MUFG to show you how using the powerful MUFG Supply Chain Finance Platform will benefit your business. </p>
+</div>
