8398c251dfeec5d06b75cb7269b73206a1ec32c3
Joe Stevens <joe.stevens@wipro.com>
2019-09-04 15:46:55 -0400

    Static Pages converted from markdown

--- src/pages/activate-login.js
+++ src/pages/activate-login.js
@@ -1,3 +1,16 @@
 import React from "react"
 
-export default () => <div>activate login</div>
+export default () => <div><h2 id="toc_0">Activate Login</h2>
+  <ul>
+  <li>Activate (First Time Use Only)</li>
+  <li>Login</li>
+  <li>Forgot Password</li>
+  </ul>
+  <p><a href="">Activate (First Time Use Only)</a></p>
+
+  <p>Honeywell Suppliers, <a href="https://www.tradepayables.com/home.html">click here</a></p>
+
+  <p>European Replenishment Suppliers, <a href="https://www.tradepayablesservices.com/tps/tpseuropecorp/en/index.html">click here</a></p>
+
+  <p>Suppliers using MUFG Platform, <a href="https://www.tradepayablesservices.com/tps/tpseuropecorp/en/index.html">click here</a></p>
+  </div>
--- src/pages/who-we-are.js
+++ src/pages/who-we-are.js
@@ -1,3 +1,20 @@
 import React from "react"
 
-export default () => <div>Who we are</div>
+export default () => <div>
+<h2 id="toc_0">Who We Are</h2>
+
+<p>As a trusted provider to corporations large and small, we build relationships with our clients that align with your business objectives and provide solutions that allow you to maximize your working capital.</p>
+
+<ul>
+<li>MUFG Supply Chain Finance</li>
+<li>MUFG Americas</li>
+</ul>
+
+<h3 id="toc_1">MUFG Supply Chain Finance</h3>
+
+<p>In the past decade, we have seen a convergence of the physical supply chain with the financial supply chain among our clients, not only multinational corporations, but also small- to medium-sized companies. With our long-standing industry expertise and deep understanding of the global supply chain, MUFG experts are ideally positioned to help both buyers and sellers tackle those challenges.</p>
+
+<h3 id="toc_2">MUFG Americas</h3>
+
+<p>We invite you to learn more about MUFG in the Americas and the range of solutions and services we offer to clients in corporate and investment banking, global trust services, transaction banking, and consumer, wealth, and commercial banking. <a href="https://mufgamericas.com">Link to Bank</a></p>
+</div>
