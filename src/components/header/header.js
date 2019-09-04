777ea49e90d6a58bfda6a5c16b36c1b2a4bb0e7a
Cecilia Song Avery <cilsong@gmail.com>
2019-09-04 14:55:55 -0400

    Feature/file structuring (#3)
    * added some file structure
    
    * git ignore
    
    * deleted cache directory
    
    * skeleton files

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Link } from 'gatsby'
+
+export default (props) => (
+  <>
+    <Link to="/who-we-are">Who We are</Link>
+    <Link to="/what-we-do">What We Do</Link>
+    <Link to="/contact-us">Contact Us</Link>
+    <Link to="/activate-login">Activate Login</Link>
+  </>
+  )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 import React from "react"
+import {Header} from '../components'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Header />
+
+)
