ef1942f394a90e95b2c743a6d8ef6a69340ac5eb
Joe Stevens <joe.stevens@wipro.com>
2019-09-05 15:49:50 -0400

    Revert "Revert "added template for layout""
    This reverts commit 3032c1f64ca13306c6eb6c02ff28ce1d238c3d83.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,11 @@ import {Link } from 'gatsby'
 
 export default (props) => (
   <>
-    <Link to="/who-we-are">Who We are</Link>
-    <Link to="/what-we-do">What We Do</Link>
-    <Link to="/contact-us">Contact Us</Link>
-    <Link to="/activate-login">Activate Login</Link>
+  <ul>
+    <li><Link to="/who-we-are">Who We are</Link></li>
+    <li><Link to="/what-we-do">What We Do</Link></li>
+    <li><Link to="/contact">Contact Us</Link></li>
+    <li><Link to="/activate-login">Activate Login</Link></li>
+    </ul>
   </>
   )
